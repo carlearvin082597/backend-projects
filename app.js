f47c8309958f971f4ce5ac66cdae0d98f2a37827
Carl Earvin Bedia Nonacido <carlnonacido@gmail.com>
2024-12-03 19:06:22 +0800

    update backend projects

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./route/admin');
 const shopRoute = require('./route/shop'); 
 

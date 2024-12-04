cba7c310b99ada04b46efee7bb7feb945b2cc9e4
Carl Earvin Bedia Nonacido <carlnonacido@gmail.com>
2024-12-05 01:18:54 +0800

    update backend projects

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+    const path = require('path');
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const expressHbs = require('express-handlebars');
 
-const app = express();
+    const app = express();
 
-app.engine('hbs', expressHbs());
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+    app.engine('handlebars', expressHbs.engine());
+    app.set('view engine', 'handlebars');
+    app.set('views', 'views');
 
-const adminData = require('./route/admin');
-const shopRoute = require('./route/shop'); 
+    const adminData = require('./route/admin');
+    const shopRoute = require('./route/shop'); 
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.route);
-app.use(shopRoute);
+    app.use('/admin', adminData.route);
+    app.use(shopRoute);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-})
+    app.use((req, res, next) => {
+        res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    })
 
-app.listen(3000);
+    app.listen(3000);
